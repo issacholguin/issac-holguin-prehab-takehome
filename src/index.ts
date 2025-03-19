5b39144634284682b7a88d7dce332096658ceca3
issacholguin <13573434+issacholguin@users.noreply.github.com>
2025-03-19 10:00:11 -0700

    express working (testing dev, build + start)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, world!");
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
